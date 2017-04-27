2923a8ffe8e022bdb33a1246df1e340146c32da5
Chris Liechti <cliechti@gmx.net>
2017-04-28 01:37:09 +0200

    feat(kernel): add more magic commands for file transfers

--- there/kernel.py
+++ there/kernel.py
@@ -21,9 +21,13 @@ Features:
 
 Original idea: Tony D! https://github.com/adafruit/jupyter_micropython_kernel
 """
+import argparse
 import shlex
+import traceback
 from ipykernel.kernelbase import Kernel
 from . import repl_connection
+from . import __main__ as commands
+import serial.tools.list_ports
 
 
 class MicroPythonKernel(Kernel):
@@ -52,10 +56,24 @@ class MicroPythonKernel(Kernel):
     def handle_meta_command(self, commandline):
         args = shlex.split(commandline)
         command = args.pop(0)
-        getattr(self, 'meta_{}'.format(command[1:]))(*args)
+        try:
+            getattr(self, 'meta_{}'.format(command[1:]))(*args)
+        except SystemExit as e:
+            self.write('mpy-repl-tool: command exited: {}'.format(e), stream='stderr')
+        except Exception as e:
+            self.write('mpy-repl-tool: internal error', stream='stderr')
+            traceback.print_exception(e, e, e.__traceback__, file=self)
 
     def meta_info(self):
-        self.write('mpy-repl-tool: connected to {}'.format(self.repl.serial))
+        if self.repl is not None:
+            self.write('mpy-repl-tool: connected to {0.port}: ' 
+                       '{0.baudrate},{0.bytesize},{0.parity},{0.stopbits}\n'.format(self.repl.serial))
+        else:
+            self.write('mpy-repl-tool: is not connected')
+
+    def meta_ports(self):
+        for info in serial.tools.list_ports.comports():
+            self.write('{}\n'.format(info))
 
     def meta_connect(self, port='hwgrep://USB', baudrate='115200'):
         baudrate = int(baudrate)
@@ -64,7 +82,20 @@ class MicroPythonKernel(Kernel):
             self.repl = None
         self.repl = repl_connection.MicroPythonRepl(port, baudrate)
         self.repl.protocol.verbose = True
-        self.write('mpy-repl-tool: connected to {}'.format(self.repl.serial))
+        self.write('mpy-repl-tool: connected to {0.port}: ' 
+                   '{0.baudrate},{0.bytesize},{0.parity},{0.stopbits}\n'.format(self.repl.serial))
+        try:
+            mpy_info = self.repl.exec('import sys; print(sys.implementation)').strip()
+        except IOError:
+            self.write('mpy-repl-tool: WARNING: version check failed! Is the port and baudrate correct?\n')
+        else:
+            self.write('mpy-repl-tool: implementation: {}\n'.format(mpy_info))
+
+    def meta_disconnect(self):
+        if self.repl is not None:
+            self.repl.close()
+            self.repl = None
+        self.write('mpy-repl-tool: diconnected')
 
     def meta_timeout(self, args=None):
         if args:
@@ -77,9 +108,61 @@ class MicroPythonKernel(Kernel):
         self.write('mpy-repl-tool: soft-reset board')
         self.repl.soft_reset()
 
-    #~ def meta_listdir(self, path):
+    def meta_ls(self, *str_args):
         #~ self.write('mpy-repl-tool: listdir {}:\n{}'.format(path, self.repl.listdir(path)))
+        parser = argparse.ArgumentParser()
+        parser.add_argument('PATH', nargs='*', default='/', help='paths to list')
+        parser.add_argument('-l', '--long', action='store_true', help='show more info')
+        parser.add_argument('-r', '--recursive', action='store_true', help='list contents of directories')
+        #~ self.write('mpy-repl-tool: {}\n'.format(str_args))
+        args = parser.parse_args(str_args)
+        commands.command_ls(self, self.repl, args)
 
+
+    def meta_cat(self, *str_args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('PATH', help='filename on target')
+        args = parser.parse_args(str_args)
+        commands.command_cat(self, self.repl, args)
+
+    def meta_pull(self, *str_args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('REMOTE', nargs='+', help='one or more source files/directories')
+        parser.add_argument('LOCAL', nargs=1, help='destination directory')
+        parser.add_argument('-r', '--recursive', action='store_true', help='copy recursively')
+        parser.add_argument('--dry-run', action='store_true', help='do not actually create anything on target')
+        args = parser.parse_args(str_args)
+        commands.command_pull(self, self.repl, args)
+
+    def meta_push(self, *str_args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('LOCAL', nargs='+', help='one or more source files/directories')
+        parser.add_argument('REMOTE', nargs=1, help='destination directory')
+        parser.add_argument('-r', '--recursive', action='store_true', help='copy recursively')
+        parser.add_argument('--dry-run', action='store_true', help='do not actually create anything on target')
+        args = parser.parse_args(str_args)
+        commands.command_push(self, self.repl, args)
+
+    def meta_cat(self, *str_args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('PATH', help='filename on target')
+        args = parser.parse_args(str_args)
+        commands.command_cat(self, self.repl, args)
+
+    def meta_rm(self, *str_args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('PATH', nargs='+', help='filename on target')
+        parser.add_argument('-f', '--force', action='store_true', help='delete anyway / no error if not existing')
+        parser.add_argument('-r', '--recursive', action='store_true', help='remove directories recursively')
+        parser.add_argument('--dry-run', action='store_true', help='do not actually create anything on target')
+        args = parser.parse_args(str_args)
+        commands.command_rm(self, self.repl, args)
+
+    def meta_df(self, *str_args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('PATH', nargs='?', default='/', help='remote path')
+        args = parser.parse_args(str_args)
+        commands.command_df(self, self.repl, args)
 
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
@@ -98,29 +181,25 @@ class MicroPythonKernel(Kernel):
             return status
         if self.repl is None:
             self.write(
-                'Please use %connect [port] [baudrate] first.\n'
-                'Defaults are first USB device and 115200 baud.',
+                'Please use "%connect [port[baudrate]]" first.\n'
+                'Defaults are first USB-serial device and 115200 baud. '
+                '"%ports" will list available serial ports.',
                 stream='stderr')
             status['status'] = 'error'
             return status
         # Run the specified code on the connected MicroPython board.
         try:
             response = self.repl.exec(code, timeout=self.timeout)
+            #~ response += self.repl.exec('if _ is not None: print(_)', timeout=3)
         except IOError as e:
             status['status'] = 'error'
             response = str(e)
             if not silent:
-                self.send_response(
-                    self.iopub_socket,
-                    'stream',
-                    {'name': 'stderr', 'text': response})
+                self.write(response, stream='stderr')
         else:
             status['status'] = 'ok'
             if not silent:
-                self.send_response(
-                    self.iopub_socket,
-                    'stream',
-                    {'name': 'stdout', 'text': response})
+                self.write(response)
         return status
 
     def do_shutdown(self, restart):
@@ -129,6 +208,29 @@ class MicroPythonKernel(Kernel):
         except:
             pass
 
+
+    # -- interface UserMessages
+
+    def output_binary(self, message):
+        """output bytes, typically stdout"""
+        self.write(message.decode('utf-8', 'replace'))   # XXX convert to hexdump or escape non printable characters??
+
+    def output_text(self, message):
+        """output text, typically stdout"""
+        self.write(message)
+
+    def error(self, message):
+        """error messages to stderr"""
+        self.write(message)
+
+    def notice(self, message):
+        """informative messages to stderr"""
+        self.write(message)
+
+    def info(self, message):
+        """informative messages to stderr, only if verbose flag is set"""
+        #~ if self.verbosity > 0:
+            #~ self.write(message)
 
 if __name__ == '__main__':
     from ipykernel.kernelapp import IPKernelApp

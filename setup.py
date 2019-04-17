cd4a9019a66fcede96d78e2443a5eb17d769706d
Chris Liechti <cliechti@gmx.net>
2019-04-17 18:31:26 +0200

    chore: release 0.10

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name="mpy-repl-tool",
     description='Communicate and transfer files from and to MicroyPython boards via REPL',
     long_description=open('README.rst').read(),
-    version='0.9',
+    version='0.10',
     author="Chris Liechti",
     author_email="cliechti@gmx.net",
     url="https://github.com/zsquareplusc/mpy-repl-tool",
